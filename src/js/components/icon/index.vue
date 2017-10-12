5c65be0a3f167df2eddc163d0fecae50ab09f6b1
Viktor Vasilev <viktor.2create@gmail.com>
2017-10-12 08:24:16 +0300

    Add icon component

---
+++ src/js/components/icon/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<span class="relative dib v-mid f0 lh-solid" :class="[width, height, padding]" v-html="icon"></span>
+</template>
+
+<script>
+	/**
+	 * The external dependencies.
+	 */
+	import feather from 'feather-icons';
+
+	export default {
+		/**
+		 * The name.
+		 *
+		 * @type {String}
+		 */
+		name: 'Icon',
+
+		/**
+		 * The exposed properties.
+		 *
+		 * @type {Object}
+		 */
+		props: {
+			name: {
+				type: String,
+				required: true
+			},
+
+			width: {
+				type: String,
+				default: 'w1'
+			},
+
+			height: {
+				type: String,
+				default: 'h1'
+			},
+
+			padding: {
+				type: String,
+				default: 'pa1'
+			}
+		},
+
+		/**
+		 * The computed properties.
+		 *
+		 * @type {Object}
+		 */
+		computed: {
+			/**
+			 * Get the icon as a SVG element.
+			 *
+			 * @return {String}
+			 */
+			icon() {
+				return feather.toSvg(this.name, { 'class': 'aspect-ratio--object' });
+			}
+		}
+	}
+</script>
