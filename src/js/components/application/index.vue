153e1dca2fda289d49af6a65737fcf16e068f2b5
Viktor Vasilev <viktor.2create@gmail.com>
2017-10-12 06:42:59 +0100

    Add header component

--- src/js/components/application/index.vue
+++ src/js/components/application/index.vue
@@ -1,18 +1,32 @@
 <template>
 	<div class="min-vh-100 bg-near-white archivo">
 		<div class="w-90 w-70-m w-50-l center">
-
+			<the-header></the-header>
 		</div>
 	</div>
 </template>
 
 <script>
+	/**
+	 * The internal dependencies.
+	 */
+	import TheHeader from 'components/the-header';
+
 	export default {
 		/**
 		 * The name.
 		 *
 		 * @type {String}
 		 */
-		name: 'Application'
+		name: 'Application',
+
+		/**
+		 * The local components.
+		 *
+		 * @type {Object}
+		 */
+		components: {
+			TheHeader
+		}
 	}
 </script>
---
+++ src/js/components/the-header/index.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<header class="pv4 flex flex-row items-center justify-start">
+		
+	</header>
+</template>
+
+<script>
+	export default {
+		/**
+		 * The name.
+		 *
+		 * @type {String}
+		 */
+		name: 'TheHeader'
+	}
+</script>
