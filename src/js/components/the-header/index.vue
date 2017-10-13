f707c2c3df24ea78ee03113e8c044ba6d0b11b64
Viktor Vasilev <viktor.2create@gmail.com>
2017-10-13 06:41:59 +0100

    Add logo component

--- src/js/components/the-header/index.vue
+++ src/js/components/the-header/index.vue
@@ -1,16 +1,30 @@
 <template lang="html">
 	<header class="pv4 flex flex-row items-center justify-start">
-		
+		<the-header-logo></the-header-logo>
 	</header>
 </template>
 
 <script>
+	/**
+	 * The internal dependencies.
+	 */
+	import TheHeaderLogo from 'components/the-header-logo';
+
 	export default {
 		/**
 		 * The name.
 		 *
 		 * @type {String}
 		 */
-		name: 'TheHeader'
+		name: 'TheHeader',
+
+		/**
+		 * The local components.
+		 *
+		 * @type {Object}
+		 */
+		components: {
+			TheHeaderLogo
+		}
 	}
 </script>
