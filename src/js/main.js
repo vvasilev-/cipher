c39a12b50dd16d8c73631c88da0d36b719b4bdc6
Viktor Vasilev <liberalcho@gmail.com>
2017-10-16 22:56:41 +0300

    Add initial integration of store

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,10 @@ import Vue from 'vue';
 /**
  * The internal dependencies.
  */
+import store from 'store';
 import Application from 'components/application';
 
 /**
  * Start the application.
  */
-new Vue(Application).$mount('#app');
+new Vue(Object.assign({ store }, Application)).$mount('#app');
---
+++ src/js/store/index.js
@@ -0,0 +1,15 @@
+/**
+ * The external dependencies.
+ */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+/**
+ * Install the plugin.
+ */
+Vue.use(Vuex);
+
+/**
+ * Initialize the store.
+ */
+export default new Vuex.Store({});
